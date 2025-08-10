8fcce5b8a38f20c84d2c6ae33f1b3b242af97022
dondarrion91 <julisanchez1991@gmail.com>
2025-08-10 18:06:59 -0300

    modify bootstrap shell

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
-export DB_HOST=172.17.0.2
+export DB_HOST=
 export DB_NAME=user_service
-export DB_USER=root
-export DB_PASS=root
+export DB_USER=
+export DB_PASS=
 export DB_PORT=3306
-export PORT=1323
+export PORT=
 
 go run cmd/userService/main.go
